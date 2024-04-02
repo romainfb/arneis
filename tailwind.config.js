3891da6f7c42486ee5912e66c10a7937f12c8764
romainfb <romainfb24pro@gmail.com>
2024-04-02 10:00:42 +0200

    This commit implements DaisyUI installation and configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,37 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        lightMode: {
+          primary: "#1f2937",
+          secondary: "#b2b5b9",
+          accent: "#f2f2f2",
+          neutral: "#fefefe",
+          "base-100": "#fefefe",
+          info: "#0000ff",
+          success: "#00ff00",
+          warning: "#00ff00",
+          error: "#ff0000",
+        },
+      },
+      {
+        darkMode: {
+          primary: "#f2f2f2",
+          secondary: "#85878c",
+          accent: "#1c232e",
+          neutral: "#1f2937",
+          "base-100": "#1f2937",
+          info: "#0000ff",
+          success: "#00ff00",
+          warning: "#00ff00",
+          error: "#ff0000",
+        },
+      },
+      "light",
+      "dark",
+    ],
+  },
 };
