43499010d422288524f861b113fdecbd487e6d97
Romain Fouillade <rfouillade@ircf.fr>
2024-05-24 13:50:57 +0200

    This commit implements vercel production

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url);
